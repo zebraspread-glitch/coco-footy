75df4a81ed477cf859e226f9df3616c9c4798abf
zebraspread-glitch <zebraspread@gmail.com>
2026-04-04 11:24:31 +1100

    fix adsense real scripttwo

--- app/layout.tsx
+++ app/layout.tsx
@@ -35,11 +35,8 @@ export default function RootLayout({
   return (
     <html lang="en">
       <head>
-  <script
-    async
-    src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=ca-pub-227605014767400"
-    crossOrigin="anonymous"
-  ></script>
+  <script async src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=ca-pub-2276050414767400"
+     crossorigin="anonymous"></script>
 </head>
       <body className={`${geistSans.variable} ${geistMono.variable} antialiased`}>
         <TopBar />
