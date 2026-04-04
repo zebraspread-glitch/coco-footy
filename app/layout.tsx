cf5e434ba308d47efd092b244dd7d0e50cad7bf8
zebraspread-glitch <zebraspread@gmail.com>
2026-04-04 11:28:24 +1100

    fix adsense script build error

--- app/layout.tsx
+++ app/layout.tsx
@@ -35,9 +35,12 @@ export default function RootLayout({
   return (
     <html lang="en">
       <head>
-  <script async src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=ca-pub-2276050414767400"
-     crossorigin="anonymous"></script>
-</head>
+        <script
+          async
+          src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=ca-pub-227605014767400"
+          crossOrigin="anonymous"
+        ></script>
+      </head>
       <body className={`${geistSans.variable} ${geistMono.variable} antialiased`}>
         <TopBar />
         {children}
